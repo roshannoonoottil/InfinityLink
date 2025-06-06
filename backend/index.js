2e65f1d654f0d68e455db0edb4240744f67d8ac6
Roshan Eldho Benny <roshannoonoottil@gmail.com>
2025-06-06 17:04:38 +0530

    Server Creation Started

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import urlRouter from './router/urlRoutes.js';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        // "https://", // ✅ Deployed frontend
+        "http://localhost:3000", // ✅ Local development frontend
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // ✅ Required for cookies/auth headers
+    exposedHeaders: ["set-cookie"] // Important for iOS
+  })
+);
+
+app.use(express.json())
+
+
+app.use('/api', urlRouter);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
