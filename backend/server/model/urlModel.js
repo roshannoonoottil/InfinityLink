8c44b377706f30caad1a426325233befc0c4a8b8
Roshan Eldho Benny <roshannoonoottil@gmail.com>
2025-06-06 20:28:51 +0530

    Update urlModel.js

--- backend/server/model/urlModel.js
+++ backend/server/model/urlModel.js
@@ -3,14 +3,14 @@ import 'dotenv/config';
 
 
 export const connectDB = async () => {
-  try {
-    await connect(process.env.MONGO_URL);
-    
-    console.log("✅ Connected to MongoDB successfully!");
-  } catch (error) {
-    console.error("❌ Error connecting to MongoDB:", error);
-    process.exit(1);
-  }
+    try {
+        await connect(process.env.MONGO_URL);
+
+        console.log("✅ Connected to MongoDB successfully!");
+    } catch (error) {
+        console.error("❌ Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
 };
 
 
